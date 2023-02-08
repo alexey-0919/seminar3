d2c74c35bfba12f1ad61bbc9585ed706129349dc
alehey <alexey.p.svyz@mail.ru>
2023-02-08 20:32:24 +0700

    add andor

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool BL(string name, bool f) {
+    System.Console.WriteLine(name);
+    return f;
+}
+
+if (BL("1", false) && BL ("2", true)) System.Console.WriteLine("3");
