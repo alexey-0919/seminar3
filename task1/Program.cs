52afdaf58e3c034fdfcea928e0a0f213f6679c8e
alehey <alexey.p.svyz@mail.ru>
2023-02-08 19:57:34 +0700

    add task 17

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) System.Console.WriteLine("1 quarter");
+else if (x < 0 && y > 0) System.Console.WriteLine("2 quarter");
+else if (x < 0 && y < 0) System.Console.WriteLine("3 quarter");
+else if (x > 0 && y < 0) System.Console.WriteLine("4 quarter");
