e16c519be9faee18f011513a3f7b8cffca84fd5b
alehey <alexey.p.svyz@mail.ru>
2023-02-08 20:10:43 +0700

    add task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,7 @@
-﻿int x = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Input x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input y");
 int y = Convert.ToInt32(Console.ReadLine());
 
 if (x > 0 && y > 0) System.Console.WriteLine("1 quarter");
